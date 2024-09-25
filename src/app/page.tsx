e28cc462ccd26ed863eb36b370048957130209b4
Jo <johj703@nate.com>
2024-09-26 01:07:22 +0900

    fix: client 컴포넌트와 server 컴포넌트에 각각 콘솔로 확인 메세지 남기기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-"use client";
-
-import { useEffect } from "react";
+import ClientExample from "../../components/ClientExample";
 
 export default function Home() {
-  useEffect(() => {
-    console.log("어디에서 실행되는 걸까요?");
-  }, []);
-  return <div className="p-8 m-4">안녕하세요! Next.js 입니다.</div>;
+  console.log("안녕, 난 서버 컴포넌트야!");
+
+  return (
+    <div className="p-8 m-4">
+      안녕하세요! Next.js 입니다.
+      <ClientExample />
+    </div>
+  );
 }
