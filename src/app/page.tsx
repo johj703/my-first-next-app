b927ad956b902c938f3ad73a6e4857e2cbb67d4a
Jo <johj703@nate.com>
2024-09-26 01:38:57 +0900

    del: 코드 초기화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import ClientExample from "../../components/ClientExample";
-
 export default function Home() {
-  console.log("안녕, 난 서버 컴포넌트야!");
-
-  return (
-    <div className="p-8 m-4">
-      안녕하세요! Next.js 입니다.
-      <ClientExample />
-    </div>
-  );
+  return <div>안녕하세요! Next.js 입니다.</div>;
 }
