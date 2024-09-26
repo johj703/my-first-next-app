36703d5287a7411660f1eb1d04355e392184c232
Jo <johj703@nate.com>
2024-09-26 15:18:55 +0900

    fix: 컴포넌트 초기화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,10 @@
-// ISR
-
 import ProductList from "./components/ProductList";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:4000/products", {
-    next: {
-      revalidate: 3,
-    },
-  });
-  const data: Product[] = await res.json();
-  console.log(data);
   return (
     <div>
+      <h1>Sparta Shop</h1>
       <ProductList />
     </div>
   );
 }
-
-export type Product = {
-  id: number;
-  handle: string;
-  availableForSale: boolean;
-  isNew: boolean;
-  title: string;
-  description: string;
-  descriptionHtml: string;
-  options: {
-    name: string;
-    values: string[];
-  }[];
-  price: {
-    amount: string;
-    currencyCode: string;
-  };
-  images: string;
-  seo: {
-    title: string;
-    description: string;
-  };
-  tags: string[];
-  rating: number;
-};
