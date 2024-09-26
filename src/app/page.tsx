17c2ad844f0c114deb1e0048448f00d6497ab6c0
Jo <johj703@nate.com>
2024-09-26 16:44:17 +0900

    fix: Suspense에서 fallback으로 Loading 컴포넌트를 사용해서 제품이 로딩될 때, Loading 컴포넌트가 나타나도록 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
 import NewProductList from "./components/NewProductList";
 import ProductList from "./components/ProductList";
+import Loading from "./loading";
 
 export default async function Home() {
   return (
     <div>
       <h1>Sparta Shop</h1>
-      <NewProductList />
+      <Suspense fallback={<Loading />}>
+        <NewProductList />
+      </Suspense>
       <ProductList />
     </div>
   );
