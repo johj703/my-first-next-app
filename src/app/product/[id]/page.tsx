eba7723bd12b3ec744b21f77db8cdd0eceaa5a91
Jo <johj703@nate.com>
2024-09-27 01:52:19 +0900

    fix: Product 페이지에 metadata 작성

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,25 @@
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = parseInt(params.id, 10);
+
+  const res = await fetch(`http://localhost:4000/products/${id}`, {
+    cache: "no-store",
+  });
+  const data: Product = await res.json();
+
+  return {
+    title: data.seo.title,
+    description: data.seo.description,
+  };
+}
+
 const ProductDetail = () => {
   return <div>ProductDetail</div>;
 };
