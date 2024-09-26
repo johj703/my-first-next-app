5877d05fe5aedcdbe7abae418b4b2d97bd665251
Jo <johj703@nate.com>
2024-09-26 17:02:53 +0900

    add: Product 디렉토리로 Loading 컴포넌트를 넣고 페이지 생성해서 테스트

---
+++ src/app/product/page.tsx
@@ -0,0 +1,12 @@
+import ProductList from "../components/ProductList";
+
+const TestPage = () => {
+  return (
+    <div>
+      TestPage
+      <ProductList />
+    </div>
+  );
+};
+
+export default TestPage;
