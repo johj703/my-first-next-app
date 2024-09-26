80da16c201dee172d521096509fbeec352307844
Jo <johj703@nate.com>
2024-09-26 16:41:45 +0900

    fix: NewProdectList를 만들고 app/page에 import하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import NewProductList from "./components/NewProductList";
 import ProductList from "./components/ProductList";
 
 export default async function Home() {
   return (
     <div>
       <h1>Sparta Shop</h1>
+      <NewProductList />
       <ProductList />
     </div>
   );
