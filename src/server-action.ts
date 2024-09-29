a107d185d0bd1a878d4657f19186b661bcb42928
Jo <johj703@nate.com>
2024-09-29 23:32:49 +0900

    fix: "cache: "force-cache""를 추가해서 static적인 페이지로 렌더링 되도록 작성

--- src/server-action.ts
+++ src/server-action.ts
@@ -2,7 +2,9 @@
 import { Product } from "@/type/product";
 
 export async function getProduct() {
-  const res = await fetch("http://localhost:4000/products", {});
+  const res = await fetch("http://localhost:4000/products", {
+    cache: "force-cache",
+  });
   const data: Product[] = await res.json();
 
   return { data };
